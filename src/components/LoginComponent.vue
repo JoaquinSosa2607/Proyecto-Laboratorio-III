e61134457cd338034ffe5cffd116b711e4845045
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2024-03-20 12:10:13 -0300

    test login

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="background-container">
+    <div class="main-container">
+      <form class="form-signin">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+          <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
+        </div>
+        <div>
+          <label for="inputName" class="sr-only mb-2">Your name</label>
+          <input
+            type="text"
+            id="inputEmail"
+            class="form-control"
+            placeholder="Name"
+            required
+            autofocus
+          />
+        </div>
+        <button class="btn btn-lg btn-secondary btn-block mt-3" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginComponent",
+};
+</script>
+
+<style>
+.background-container {
+  background-image: url("../assets/bitcoin-background.jpg");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main-container {
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 10px;
+  padding: 40px;
+  width: 90%;
+  max-width: 500px;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+.icon {
+  width: 50px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      class="container d-flex justify-content-center align-items-center h-100"
+    >
+      <div class="card text-center shadow">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="true" href="#"
+                >Sign In</a
+              >
+            </li>
+            <li class="nav-item">
+              <router-link to="/about">
+              <a class="nav-link active">About</a>
+            </router-link>
+            </li>
+          </ul>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">
+            Hi! You're about to enter <strong>Sossen Crypto Exchange</strong>
+          </h5>
+          <div class="card-btn">
+            <router-link to="/login">
+              <button class="btn btn-secondary btn-lg">Sign In</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView"
+};
 </script>
+
+<style>
+.home {
+  height: 100vh;
+  background-image: url("../assets/bitcoin-background.jpg");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+}
+
+.card-body {
+  height: 200px;
+}
+
+.card-title {
+  margin-top: 20px;
+}
+
+.card-btn {
+  margin-top: 50px;
+}
+</style>
